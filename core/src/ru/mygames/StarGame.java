0db3629d2b1b9d9b25484520959ce77dfa47b140
Ivan <ivan_semenov@bk.ru>
2021-07-31 19:04:42 +0300

    Lesson 2 part 1

--- core/src/ru/mygames/StarGame.java
+++ core/src/ru/mygames/StarGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class StarGame extends ApplicationAdapter {
@@ -21,7 +22,43 @@ public class StarGame extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		background = new Texture("imageForHW1.jpg");
-		region = new TextureRegion(img, 25, 25, 100, 50);
+		//region = new TextureRegion(img, 25, 25, 100, 50);
+
+		Vector2 v1 = new Vector2(3,2);
+		Vector2 v2 = new Vector2();
+		v2.set(2,1);
+
+		v1.add(v2);
+		System.out.println(String.format("v1.add(v2), v1.x = %s, v1.y = %s", v1.x, v1.y));
+
+		v1.set(7, 7);
+		v2.set(3, 1);
+		v1.sub(v2);
+		System.out.println(String.format("v1.sub(v2), v1.x = %s, v1.y = %s", v1.x, v1.y));
+		System.out.println("v1.len() = " + v1.len());
+		v1.scl(0.5f); // скалирование. Умножение вектора на скаляр
+		System.out.println("v1.len() = after scl " + v1.len());
+
+		v1.nor(); // нормализация. Приведение вектора к единичной длине. Например для участия в расчетах
+		// где нужно учитывать направление, но не влиять на величину другого вектора
+		System.out.println("v1.len() = after nor " + v1.len());
+
+		v2.cpy(); // используется когда нужна операция над копией вектора, но без изменения
+		// исходного вектора
+		// опасый метод, т.к. если попадает в render, то 60 раз в секунду будет создаваться новый
+		// объект
+		System.out.println("v2.x = " + v2.x + "v2.y = " + v2.y);
+		v2.cpy().add(v1);
+		System.out.println("v2.x = " + v2.x + "v2.y = " + v2.y);
+
+		// Скалярное произведение векторов. Используется для определения угла между векторами.
+
+		v1.set(1, 1);
+		v2.set(-1, 1);
+		v1.nor();
+		v2.nor();
+		System.out.println(Math.acos(v1.dot(v2)));
+
 	}
 
 	@Override
